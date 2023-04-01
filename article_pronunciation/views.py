6b4c4423f311789570351cf799f91abaf345411d
居正中 <AlanJui.1960@gmail.com>
2023-04-01 21:19:07 +0800

    完成可以切換「漢字」標註讀音所使用的「注音符號」。

--- article_pronunciation/views.py
+++ article_pronunciation/views.py
@@ -7,18 +7,54 @@ from django.urls import reverse
 from han_ji_dict.models import HanJi
 
 
+def get_chu_im(char, pronunciation):
+    han_ji_instances = HanJi.objects.filter(han_ji=char, freq__isnull=False).order_by(
+        '-freq'
+    )
+    if han_ji_instances.exists():
+        han_ji_instance = han_ji_instances[0]
+        if pronunciation == "tai_lo":
+            return han_ji_instance.get_tai_lo_chu_im()
+        elif pronunciation == "peh_oe_ji":
+            return han_ji_instance.get_peh_oe_ji_chu_im()
+        elif pronunciation == "ban_phing":
+            return han_ji_instance.get_ban_phing_chu_im()
+        elif pronunciation == "sip_ngoo":
+            return han_ji_instance.get_sip_ngoo_im_chu_im()
+        elif pronunciation == "fong_yim":
+            return han_ji_instance.get_fong_yim_fu_ho_chu_im()
+    return ''
+
+
 def index(request):
     default_text = "我可以為您標註漢字讀音"
-    annotated_text = ""
+    annotated_text = []
 
-    if request.method == "POST":
-        input_text = request.POST.get("text", default_text)
-        annotated_text = annotate_pronunciation(input_text)
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        selected_pronunciation = request.POST.get('pronunciation')
+        if not selected_pronunciation:
+            selected_pronunciation = "tai_lo"
 
-    context = {
-        "default_text": default_text,
-        "annotated_text": annotated_text,
-    }
+        if text:
+            for char in text:
+                if char.strip():
+                    chu_im_fu_ho = get_chu_im(char, selected_pronunciation)
+                    annotated_text.append(
+                        {
+                            'han_ji': char,
+                            'chu_im_fu_ho': chu_im_fu_ho,
+                        }
+                    )
+        context = {
+            "default_text": default_text,
+            "annotated_text": annotated_text,
+            "selected_pronunciation": selected_pronunciation,
+        }
+    else:
+        context = {
+            "default_text": default_text,
+        }
 
     return render(request, "article_pronunciation/index.html", context)
 
