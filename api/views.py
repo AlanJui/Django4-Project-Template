f4984cf934d0d7a039cc0eb1a91d9ac4da50e084
居正中 <AlanJui.1960@gmail.com>
2023-06-25 21:05:19 +0800

    完成 API：GET /huan_tshiat_huat/

--- api/views.py
+++ api/views.py
@@ -10,9 +10,30 @@ from han_ji_dict.models import HanJi
 # ==========================================================
 # 反切法推導漢字讀音
 # http://localhost:8000/api/huan_tshiat_huat/?han_ji=東
+# http://localhost:8000/api/huan_tshiat_huat/?han_ji=在
 # ==========================================================
 @api_view(['GET'])
 def huan_tshiat_huat(request):
+    if 'han_ji' in request.query_params:
+        # 取得欲查詢之「漢字」
+        han_ji = request.query_params['han_ji']
+        # 以漢字查詢「反切讀音」
+        han_ji_dict = tsa_huan_tshiat(han_ji)
+        tak_im_list = han_ji_dict['tak_im_list']
+        for tak_im in tak_im_list:
+            siong_ji = tak_im['huan_tshiat'][0]
+            e_ji = tak_im['huan_tshiat'][1:]
+            tak_im['piau_im'] = parse_fanqie(siong_ji, e_ji)
+        return Response({
+            'han_ji': han_ji,
+            'tak_im_list': tak_im_list,
+        })
+    else:
+        return Response({'error': '請提供要查詢的漢字。'})
+
+
+@api_view(['GET'])
+def huan_tshiat_huat2(request):
     # return Response({'message': '你好，世界！'})
     if 'han_ji' in request.query_params:
         han_ji = request.query_params['han_ji']
@@ -20,3 +41,8 @@ def huan_tshiat_huat(request):
         return Response({'han_ji': han_ji, 'huan_tshiat': result})
     else:
         return Response({'error': '請提供要查詢的漢字。'})
+
+
+@api_view(['GET'])
+def hello(request):
+    return Response({'message': '你好，世界！'})
--- article_pronunciation/lib_tsa_huan_tshiat.py
+++ article_pronunciation/lib_tsa_huan_tshiat.py
@@ -5,7 +5,21 @@ from bs4 import BeautifulSoup
 # 使用《中國哲學書電子化計劃》的《漢字字典》查「讀音」
 # url = 'https://ctext.org/dictionary.pl?if=gb&char=東'
 # url = 'https://ctext.org/dictionary.pl?if=gb&char=在'
-def extract_readings_from_html(han_ji, html):
+def convert_list_to_dict_list(string_list):
+    dict_list = []
+    for i in range(0, len(string_list), 5):
+        han_ji_tak_im = {
+            'huan_tshiat': string_list[i],
+            'un_su': string_list[i + 1] if i + 1 < len(string_list) else None,
+            'siann_tiau': string_list[i + 2] if i + 2 < len(string_list) else None,
+            'siann_bu': string_list[i + 3] if i + 3 < len(string_list) else None,
+            'un_bu': string_list[i + 4] if i + 4 < len(string_list) else None
+        }
+        dict_list.append(han_ji_tak_im)
+    return dict_list
+
+
+def extract_readings_from_html(html):
     # 自「網頁」中的「表格（Table）」，查找「反切」欄位，及取出「漢字讀音」
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find('table', class_='info')
@@ -22,29 +36,8 @@ def extract_readings_from_html(han_ji, html):
                         reading = content.text.strip()
                         reading_list.append(reading)
                 if reading_list:
-                    for i in range(len(reading_list)):
-                        if i % 5 == 0:
-                            reading = {
-                                'han_ji': han_ji,
-                                'huan_tshiat': None,
-                                'un_su': None,
-                                'siann_tiau': None,
-                                'siann_bu': None,
-                                'un_bu': None,
-                            }
-                        if i % 5 == 0:
-                            reading['huan_tshiat'] = reading_list[i]
-                        elif i % 5 == 1:
-                            reading['un_su'] = reading_list[i]
-                        elif i % 5 == 2:
-                            reading['siann_tiau'] = reading_list[i]
-                        elif i % 5 == 3:
-                            reading['siann_bu'] = reading_list[i]
-                        elif i % 5 == 4:
-                            reading['un_bu'] = reading_list[i]
-
-                        if i % 5 == 4:
-                            readings.append(reading)
+                    dict_list = convert_list_to_dict_list(reading_list)
+                    readings.extend(dict_list)
     # 若輸入之「漢字」查找不到「反切」的讀音，則 readings 為空
     return readings
 
@@ -55,53 +48,48 @@ def tsa_huan_tshiat(han_ji):
     response = requests.get(url)
 
     # 解析 HTTP Response 返回的網頁（HTML）內容
-    readings = extract_readings_from_html(han_ji, response.text)
-    results = []
-    for reading in readings:
-        result = {
-            'han_ji': han_ji,
-            'huan_tshiat': reading['huan_tshiat'],
-            'un_su': reading['un_su'],
-            'siann_tiau': reading['siann_tiau'],
-            'siann_bu': reading['siann_bu'],
-            'un_bu': reading['un_bu']
-        }
-        results.append(result)
-    return results
+    han_ji_dict = {
+        'han_ji': han_ji,
+        'tak_im_list': None,
+    }
+    han_ji_dict['tak_im_list'] = extract_readings_from_html(response.text)
+    return han_ji_dict
 
 
 def run_test(han_ji):
-    results = tsa_huan_tshiat(han_ji)
-    if results:
-        for result in results:
-            print(f'【查找漢字】：{han_ji}')
-            print(f'【反切讀音】：{result["huan_tshiat"]}')
-            print(f'【查詢結果】：{result}')
+    han_ji_dict = tsa_huan_tshiat(han_ji)
+    if len(han_ji_dict['tak_im_list']) > 0:
+        print('==============================================')
+        print(f'【查找漢字】：{han_ji_dict["han_ji"]}')
+        for tak_im in han_ji_dict['tak_im_list']:
+            print('----------------------------------------------')
+            print(f'【反切讀音】：{tak_im["huan_tshiat"]}')
+            print(f'【查詢結果】：{tak_im}')
     else:
         print('找不到反切讀音')
     print()
 
 
 # 驗證用例
-han_ji_thak_im = {
-    'han_ji': '在',
-    'huan_tshiat': '昨宰',
-    'un_su': '廣韻',
-    'siann_tiau': '上聲',
-    'siann_bu': '海',
-    'un_bu': '在',
+# han_ji_thak_im = {
+#     'han_ji': '在',
+#     'huan_tshiat': '昨宰',
+#     'un_su': '廣韻',
+#     'siann_tiau': '上聲',
+#     'siann_bu': '海',
+#     'un_bu': '在',
 
 
-}
-han_ji_thak_im2 = {
-    'han_ji': '在',
-    'huan_tshiat': '昨代',
-    'un_su': '廣韻',
-    'siann_tiau': '去聲',
-    'siann_bu': '代',
-    'un_bu': '載',
-}
-han_ji_thak_im_list = [han_ji_thak_im, han_ji_thak_im2]
+# }
+# han_ji_thak_im2 = {
+#     'han_ji': '在',
+#     'huan_tshiat': '昨代',
+#     'un_su': '廣韻',
+#     'siann_tiau': '去聲',
+#     'siann_bu': '代',
+#     'un_bu': '載',
+# }
+# han_ji_thak_im_list = [han_ji_thak_im, han_ji_thak_im2]
 
-run_test('在')
-run_test('東')
+# run_test('在')
+# run_test('東')
