bfa65f9734f08cef3babda583cbfd9006f6f33b9
居正中 <AlanJui.1960@gmail.com>
2023-06-26 11:55:26 +0800

    使用 API ：以「漢字」查找「反切讀音」及「台羅拼音」。

--- article_pronunciation/lib_tsa_huan_tshiat.py
+++ article_pronunciation/lib_tsa_huan_tshiat.py
@@ -1,6 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
 
+from han_ji_dict.models import HanJi
+
+
+def determine_tone(tiau1, tiau2):
+    # determine the clearness/muddiness
+    clearness = 'clear' if int(tiau1) <= 4 else 'muddy'
+
+    # determine tone category
+    if int(tiau2) in {1, 5}:
+        tone_category = 'level'
+    elif int(tiau2) in {2, 6}:
+        tone_category = 'rising'
+    elif int(tiau2) in {3, 7}:
+        tone_category = 'departing'
+    elif int(tiau2) in {4, 8}:
+        tone_category = 'entering'
+    else:
+        return "Invalid tone for the lower character."
+
+    # get the tone
+    if clearness == 'clear':
+        if tone_category == 'level':
+            tone = '1'
+        elif tone_category == 'rising':
+            tone = '2'
+        elif tone_category == 'departing':
+            tone = '3'
+        else:  # entering
+            tone = '4'
+    else:  # muddy
+        if tone_category == 'level':
+            tone = '5'
+        elif tone_category == 'rising':
+            tone = '6'
+        elif tone_category == 'departing':
+            tone = '7'
+        else:  # entering
+            tone = '8'
+
+    return tone
+
+
+# ==========================================================
+# 反切法推導漢字讀音
+# ==========================================================
+def parse_fanqie(character1, character2):
+    try:
+        han_ji1 = HanJi.objects.get(han_ji=character1)
+        han_ji2 = HanJi.objects.get(han_ji=character2)
+    except HanJi.DoesNotExist:
+        return None
+
+    siann_bu1, un_bu1, tiau1 = han_ji1.split_chu_im()
+    siann_bu2, un_bu2, tiau2 = han_ji2.split_chu_im()
+
+    tone = determine_tone(tiau1, tiau2)
+
+    pinyin = siann_bu1 + un_bu2 + tone
+    return pinyin
+
 
 # 使用《中國哲學書電子化計劃》的《漢字字典》查「讀音」
 # url = 'https://ctext.org/dictionary.pl?if=gb&char=東'
@@ -57,13 +117,24 @@ def tsa_huan_tshiat(han_ji):
 
 
 def run_test(han_ji):
+    # 以「漢字」查「反切讀音」
     han_ji_dict = tsa_huan_tshiat(han_ji)
+    # 以「漢字反切讀音」查詢結果，透過「漢字典」查找「音標」
+    for tak_im in han_ji_dict['tak_im_list']:
+        # 從「反切讀音」欄位，取「上字」（聲母）、「下字」（韻母）
+        siong_im = tak_im['huan_tshiat'][0]
+        e_ji = tak_im['huan_tshiat'][1:]
+        # 以「上字」、「下字」查「音標」；再將「音標」填入「反切讀音」欄位
+        tak_im['piau_im'] = parse_fanqie(siong_im, e_ji)
+
+    # 顯示查詢結果
     if len(han_ji_dict['tak_im_list']) > 0:
         print('==============================================')
         print(f'【查找漢字】：{han_ji_dict["han_ji"]}')
         for tak_im in han_ji_dict['tak_im_list']:
             print('----------------------------------------------')
             print(f'【反切讀音】：{tak_im["huan_tshiat"]}')
+            print(f'【拼音字母】：{tak_im["piau_im"]}')
             print(f'【查詢結果】：{tak_im}')
     else:
         print('找不到反切讀音')
@@ -71,6 +142,9 @@ def run_test(han_ji):
 
 
 # 驗證用例
+run_test('在')
+# run_test('東')
+
 # han_ji_thak_im = {
 #     'han_ji': '在',
 #     'huan_tshiat': '昨宰',
@@ -90,6 +164,3 @@ def run_test(han_ji):
 #     'un_bu': '載',
 # }
 # han_ji_thak_im_list = [han_ji_thak_im, han_ji_thak_im2]
-
-# run_test('在')
-# run_test('東')
