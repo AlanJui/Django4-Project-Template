5bbff8499fe1d54c57bc8a8f8790ab487e140c76
居正中 <AlanJui.1960@gmail.com>
2023-07-03 14:01:02 +0800

    完成「以漢字找讀音 API 」：台羅拼音、十五音、白話字、閩拚、方音符號。

--- api/views.py
+++ api/views.py
@@ -32,7 +32,7 @@ def huan_tshiat_huat(request):
 
 # ==========================================================
 # 查找漢字發音
-# http://localhost:8000/api/tshue_han_ji_huat_im/?han_ji=東
+# http://localhost:8000/api/tshue_han_ji_huat_im/在/
 # ==========================================================
 @api_view(['GET'])
 def tshue_han_ji_huat_im(request, han_ji):  # noqa: unused-argument
@@ -52,7 +52,7 @@ def tshue_han_ji_huat_im(request, han_ji):  # noqa: unused-argument
 
 # ==========================================================
 # 查找漢字十五音標音發音
-# http://localhost:8000/api/tshue_han_ji_huat_im/?han_ji=東
+# http://localhost:8000/api/tshue_han_ji_huat_im/sip_ngoo_im/東/
 # ==========================================================
 @api_view(['GET'])
 def tshue_han_ji_e_sip_ngoo_im(request, han_ji):
@@ -74,7 +74,7 @@ def tshue_han_ji_e_sip_ngoo_im(request, han_ji):
 
 # ==========================================================
 # 查找漢字台羅拼音發音
-# http://localhost:8000/api/tshue_han_ji_huat_im/?han_ji=東
+# http://localhost:8000/api/tshue_han_ji_huat_im/tai_lo_phing_im/東/
 # ==========================================================
 @api_view(['GET'])
 def tshue_han_ji_e_tai_lo_phing_im(request, han_ji):
@@ -88,7 +88,73 @@ def tshue_han_ji_e_tai_lo_phing_im(request, han_ji):
     if u_tshue_tioh_e_han_ji:
         return Response({
             'han_ji': han_ji,
-            'sip_ngoo_im': u_tshue_tioh_e_han_ji.get_tai_lo_chu_im(),
+            'tai_lo_phing_im': u_tshue_tioh_e_han_ji.get_tai_lo_chu_im(),
+        })
+    else:
+        return Response({'error': '請提供要查詢的漢字。'})
+
+
+# ==========================================================
+# 查找白話字（POJ）拼音發音
+# http://localhost:8000/api/tshue_han_ji_huat_im/peh_ue_ji/東/
+# ==========================================================
+@api_view(['GET'])
+def tshue_han_ji_e_peh_ue_ji(request, han_ji):
+    # 對 request 參數進行一個操作，以顯示使用它
+    _ = request.GET.get('example_param')
+
+    # 取得欲查詢之「漢字」
+    u_tshue_tioh_e_han_ji = HanJi.objects.filter(
+        han_ji=han_ji).order_by('-freq').first()
+
+    if u_tshue_tioh_e_han_ji:
+        return Response({
+            'han_ji': han_ji,
+            'peh_ue_ji': u_tshue_tioh_e_han_ji.get_peh_oe_ji_chu_im(),
+        })
+    else:
+        return Response({'error': '請提供要查詢的漢字。'})
+
+
+# ==========================================================
+# 查找漢字閩拼（BP）發音
+# http://localhost:8000/api/tshue_han_ji_huat_im/ban_phing/東/
+# ==========================================================
+@api_view(['GET'])
+def tshue_han_ji_e_ban_phing(request, han_ji):
+    # 對 request 參數進行一個操作，以顯示使用它
+    _ = request.GET.get('example_param')
+
+    # 取得欲查詢之「漢字」
+    u_tshue_tioh_e_han_ji = HanJi.objects.filter(
+        han_ji=han_ji).order_by('-freq').first()
+
+    if u_tshue_tioh_e_han_ji:
+        return Response({
+            'han_ji': han_ji,
+            'ban_phing': u_tshue_tioh_e_han_ji.get_ban_phing_chu_im(),
+        })
+    else:
+        return Response({'error': '請提供要查詢的漢字。'})
+
+
+# ==========================================================
+# 查找漢字方音符號（TPS）發音
+# http://localhost:8000/api/tshue_han_ji_huat_im/hong_im_fu_ho/東/
+# ==========================================================
+@api_view(['GET'])
+def tshue_han_ji_e_hong_im_hu_ho(request, han_ji):
+    # 對 request 參數進行一個操作，以顯示使用它
+    _ = request.GET.get('example_param')
+
+    # 取得欲查詢之「漢字」
+    u_tshue_tioh_e_han_ji = HanJi.objects.filter(
+        han_ji=han_ji).order_by('-freq').first()
+
+    if u_tshue_tioh_e_han_ji:
+        return Response({
+            'han_ji': han_ji,
+            'hong_im_fu_ho': u_tshue_tioh_e_han_ji.get_fong_yim_fu_ho_chu_im(),
         })
     else:
         return Response({'error': '請提供要查詢的漢字。'})
