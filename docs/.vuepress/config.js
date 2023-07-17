88523212551a01165bc1077714b573ddbc308fad
居正中 <AlanJui.1960@gmail.com>
2023-07-17 12:03:24 +0800

    加入 SSH 操作指引。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,23 @@
-const { description } = require('../../package')
+const { description } = require("../../package")
 
 module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  base: '/han_gi_chu_im/',
+  base: "/han_gi_chu_im/",
   locales: {
-    '/': {
-      lang: 'zh-TW',
+    "/": {
+      lang: "zh-TW",
     },
   },
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: '《專案文件舘》',
+  title: "《專案文件舘》",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
-  description: '彙集專案相關文件',
+  description: "彙集專案相關文件",
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
@@ -25,11 +25,11 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     [
-      'meta',
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
     ],
   ],
 
@@ -39,10 +39,10 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: '',
+    repo: "",
     editLinks: false,
-    docsDir: '',
-    editLinkText: '',
+    docsDir: "",
+    editLinkText: "",
     lastUpdated: false,
     // nav: [
     //   { text: '首頁', link: '/' },
@@ -53,204 +53,208 @@ module.exports = {
     // ],
     nav: [
       {
-        text: 'Guide',
-        link: '/guide/',
+        text: "Guide",
+        link: "/guide/",
       },
       {
-        text: 'Config',
-        link: '/config/',
+        text: "Config",
+        link: "/config/",
       },
       {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org',
+        text: "VuePress",
+        link: "https://v1.vuepress.vuejs.org",
       },
     ],
     sidebar: [
       {
-        title: '作業指引',
+        title: "作業指引",
         collapsable: false,
         children: [
           {
-            title: '使用者需求',
-            path: '/d100_URS/',
+            title: "使用者需求",
+            path: "/d100_URS/",
           },
           {
-            title: '系統需求',
-            path: '/d200_SRS/',
+            title: "系統需求",
+            path: "/d200_SRS/",
           },
           {
-            title: '系統設計',
-            path: '/d300_SDD/',
+            title: "系統設計",
+            path: "/d300_SDD/",
           },
           {
-            title: '開發與測試',
-            path: '/d400_DEV/',
+            title: "開發與測試",
+            path: "/d400_DEV/",
           },
           {
-            title: '系統維運',
-            path: '/d500_OP/',
+            title: "系統維運",
+            path: "/d500_OP/",
           },
           {
-            title: '參考資料',
-            path: '/d900_REF/',
+            title: "參考資料",
+            path: "/d900_REF/",
           },
           {
-            title: '作業指引',
-            path: '/guide/',
+            title: "作業指引",
+            path: "/guide/",
           },
         ],
       },
       {
-        title: '電腦硬體',
+        title: "電腦硬體",
         collapsable: true,
         children: [
           {
-            title: '伺服器（SRV-2020）',
-            path: '/_my_dev_env/srv-2020/',
+            title: "伺服器（SRV-2020）",
+            path: "/_my_dev_env/srv-2020/",
             collapsable: true,
             sidebarDepth: 2,
           },
           {
-            title: '開發機（MBP-2018）',
-            path: '/_my_dev_env/MBP-2018/',
+            title: "開發機（MBP-2018）",
+            path: "/_my_dev_env/MBP-2018/",
             collapsable: true,
             sidebarDepth: 2,
           },
         ],
       },
       {
-        title: '開發環境',
+        title: "開發環境",
         collapsable: true,
         children: [
           {
-            title: 'Python',
+            title: "Python",
             collapsable: true,
             children: [
               {
-                title: 'Python',
-                path: '/_my_dev_env/python/',
+                title: "Python",
+                path: "/_my_dev_env/python/",
               },
               {
-                title: 'pyenv',
-                path: '/_my_dev_env/python/pyenv.md',
+                title: "pyenv",
+                path: "/_my_dev_env/python/pyenv.md",
               },
               {
-                title: 'poetry',
-                path: '/_my_dev_env/python/poetry.md',
+                title: "poetry",
+                path: "/_my_dev_env/python/poetry.md",
               },
               {
-                title: 'Django',
-                path: '/_my_dev_env/django/',
+                title: "Django",
+                path: "/_my_dev_env/django/",
               },
             ],
           },
           {
-            title: 'Node.js',
-            path: '/_my_dev_env/nodejs/',
+            title: "Node.js",
+            path: "/_my_dev_env/nodejs/",
           },
           {
-            title: 'Lua',
-            path: '/_my_dev_env/lua/',
+            title: "Lua",
+            path: "/_my_dev_env/lua/",
           },
           {
-            title: 'Docker',
-            path: '/_my_dev_env/docker/',
-          },
-        ],
-      },
-      {
-        title: '常用工具',
-        collapsable: true,
-        children: [
-          {
-            title: 'Yabai',
-            path: '/_my_dev_env/yabai/',
-          },
-          {
-            title: 'Alacritty',
-            path: '/_my_dev_env/terminal/alacritty',
-          },
-          {
-            title: 'Tmux',
-            path: '/_my_dev_env/terminal/tmux',
-          },
-          {
-            title: 'VuePress',
-            path: '/_my_dev_env/vuepress/',
-          },
-          {
-            title: 'Neovim',
-            path: '/_my_dev_env/nvim/',
-          },
-          {
-            title: 'VS Code',
-            path: '/_my_dev_env/VSCode/',
-          },
-          {
-            title: 'Git',
-            path: '/_my_dev_env/git/',
-          },
-          {
-            title: 'VirtualBox',
-            path: '/_my_dev_env/VirtualBox/',
-          },
-          {
-            title: 'PostgreSQL',
-            path: '/_my_dev_env/postgres/',
-          },
-          {
-            title: 'Vim',
-            path: '/_my_dev_env/vim/',
+            title: "Docker",
+            path: "/_my_dev_env/docker/",
           },
         ],
       },
       {
-        title: '參考資料',
+        title: "常用工具",
         collapsable: true,
         children: [
           {
-            title: '快速參考',
-            path: '/_my_dev_env/QuickRef/',
+            title: "SSH",
+            path: "/_my_dev_env/terminal/ssh",
           },
           {
-            title: 'Manjaro KDE 開發環境',
+            title: "Yabai",
+            path: "/_my_dev_env/yabai/",
+          },
+          {
+            title: "Git",
+            path: "/_my_dev_env/git/",
+          },
+          {
+            title: "Alacritty",
+            path: "/_my_dev_env/terminal/alacritty",
+          },
+          {
+            title: "Tmux",
+            path: "/_my_dev_env/terminal/tmux",
+          },
+          {
+            title: "Neovim",
+            path: "/_my_dev_env/nvim/",
+          },
+          {
+            title: "VuePress",
+            path: "/_my_dev_env/vuepress/",
+          },
+          {
+            title: "PostgreSQL",
+            path: "/_my_dev_env/postgres/",
+          },
+          {
+            title: "VS Code",
+            path: "/_my_dev_env/VSCode/",
+          },
+          // {
+          //   title: 'VirtualBox',
+          //   path: '/_my_dev_env/VirtualBox/',
+          // },
+          {
+            title: "Vim",
+            path: "/_my_dev_env/vim/",
+          },
+        ],
+      },
+      {
+        title: "參考資料",
+        collapsable: true,
+        children: [
+          {
+            title: "快速參考",
+            path: "/_my_dev_env/QuickRef/",
+          },
+          {
+            title: "Manjaro KDE 開發環境",
             collapsable: true,
             children: [
               {
-                title: 'Manjaro KDE 摘要',
-                path: '/_ManjaroKDE/',
+                title: "Manjaro KDE 摘要",
+                path: "/_ManjaroKDE/",
                 collapsable: true,
                 sidebarDepth: 2,
               },
               {
-                title: '作業系統安裝',
-                path: '/_ManjaroKDE/D01_OS/',
+                title: "作業系統安裝",
+                path: "/_ManjaroKDE/D01_OS/",
                 collapsable: true,
                 sidebarDepth: 2,
               },
               {
-                title: '終端機作業',
-                path: '/_ManjaroKDE/D02_Terminal/',
+                title: "終端機作業",
+                path: "/_ManjaroKDE/D02_Terminal/",
                 collapsable: true,
                 sidebarDepth: 2,
               },
               {
-                title: '開發環境建置',
-                path: '/_ManjaroKDE/D03_DevEnvironments/',
+                title: "開發環境建置",
+                path: "/_ManjaroKDE/D03_DevEnvironments/",
                 collapsable: true,
                 sidebarDepth: 2,
               },
               {
-                title: '開發作業工具',
-                path: '/_ManjaroKDE/D04_DevTools/',
+                title: "開發作業工具",
+                path: "/_ManjaroKDE/D04_DevTools/",
                 collapsable: true,
                 sidebarDepth: 2,
               },
             ],
           },
           {
-            title: '其它⋯⋯',
-            path: '/_my_dev_env/misc/',
+            title: "其它⋯⋯",
+            path: "/_my_dev_env/misc/",
           },
         ],
       },
@@ -261,9 +265,9 @@ module.exports = {
     extendMarkdown: (md) => {
       // md.use(require('markdown-it-prism'))
       // 使用 markdown-it-mermaid 插件
-      md.use(require('markdown-it-mermaid').default)
+      md.use(require("markdown-it-mermaid").default)
       // 控制圖片縮放
-      md.use(require('markdown-it-imsize'))
+      md.use(require("markdown-it-imsize"))
     },
   },
 
@@ -271,11 +275,11 @@ module.exports = {
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
-    '@vuepress/plugin-palette',
+    "@vuepress/plugin-palette",
     {
-      preset: 'sass', // 使用 Sass 預設顏色方案
+      preset: "sass", // 使用 Sass 預設顏色方案
     },
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
+    "@vuepress/plugin-back-to-top",
+    "@vuepress/plugin-medium-zoom",
   ],
 }
